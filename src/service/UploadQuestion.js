c2ae2ad36c5e7ade0dc7dd9bbb0b1bf495ae557b
Brandon Villagran <bvillagran@ibm.com>
2021-03-22 16:37:30 -0400

    connect to grant-spring-demo

--- src/service/UploadQuestion.js
+++ src/service/UploadQuestion.js
@@ -1,15 +1,12 @@
 export default async function uploadQuestion(body) {
   try {
-    const response = await fetch(
-      'http://localhost:8080/add-question',
-      {
-        method: 'POST',
-        body: JSON.stringify(body),
-        header: {
-          'Content-Type': 'application/json'
-        }
+    const response = await fetch("http://grant-spring-demo:8080/add-question", {
+      method: "POST",
+      body: JSON.stringify(body),
+      header: {
+        "Content-Type": "application/json",
       },
-    );
+    });
     return await response.json();
   } catch (e) {
     console.log(e);
